084d80e159dfcf78a2f8ace57f1db70a8ab0d78e
Davi Lucas <davilucasdlcreator@gmail.com>
2020-12-16 19:53:29 -0300

    Criação de Componente que indica ação de algum usuario como curtidas...

--- components/UserAction/index.js
+++ components/UserAction/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Text , View, Image, TouchableOpacity} from 'react-native';
 
-export default function UserAction({ name, url, description }){
+export default function UserAction({ name, url, description ,onPress=()=>{},style={} }){
     return(
-      <TouchableOpacity>
+      <TouchableOpacity style={style} onPress={onPress}>
         <View style={{flex:1,alignItems:'center', flexDirection:'row',margin:10,padding:5}}> 
           <Image source={{uri:url}} style={{marginHorizontal:10,height:50,width:50,borderRadius:25,borderWidth:1.6,borderColor:'#da2d79'}} />
           <View style={{marginHorizontal:10}}>
             <Text style={{fontSize:17}}>{name}</Text>
-            <Text style={{fontSize:14}}>{description}</Text>
+            <Text style={{fontSize:13,color:'#555'}}>{description}</Text>
           </View>
         </View>
       </TouchableOpacity>
