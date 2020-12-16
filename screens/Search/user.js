06f79adea0f7f7bce7f41fdd92b9a65741ba64d6
Davi Lucas <davilucasdlcreator@gmail.com>
2020-12-16 19:50:59 -0300

    Adição de Posts que talvez o usuario goste

--- screens/Search/user.js
+++ screens/Search/user.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { ScrollView } from 'react-native'
 import InstaGithub from '../../components/Posts/post';
+import Posts from '../../components/Posts'
 
 export default function User({ route }){
     const githubUser = route.params
     return (
-      <InstaGithub githubUser={githubUser}/>
+      <ScrollView>
+        <InstaGithub githubUser={githubUser}/>
+        <Posts />
+      </ScrollView>
     )
   }
